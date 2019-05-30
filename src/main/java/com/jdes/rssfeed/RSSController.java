cf26909319a090f1e96491c7a45c2e2e7e3c2bd6
Johnboi <johndesmond631@gmail.com>
2019-05-30 15:36:25 -0500

    successfully updated db from controller

--- src/main/java/com/jdes/rssfeed/RSSController.java
+++ src/main/java/com/jdes/rssfeed/RSSController.java
@@ -1,8 +1,10 @@
 package com.jdes.rssfeed;
 import org.springframework.stereotype.Controller;
+
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
@@ -24,17 +26,54 @@ public class RSSController {
     return "sample";
   }
 
-@Autowired
+@Autowired // get the bean called articleRepository 
 private ArticleRepository articleRepository;
 
 // get articles
 // get a single article
 // delete an article 
 
-@GetMapping("/articles")
-public List<Article> getAllArticles() {
-	return articleRepository.findAll();
-}
+//@GetMapping("/articles")
+//public List<Article> getAllArticles() {
+//	return articleRepository.findAll();
+//		
+//	}
+
+@Autowired 
+SourceRepository sourceRepository;
+
+@GetMapping("/sourceadd")
+public @ResponseBody String insertToSource(@RequestParam String url) {
+	// ok how do I get the html form to 
 	
+	RSSFeedParser newSource = new RSSFeedParser(url);
+	// This will return a feed [ITEM, TITLE, DESCRIPTION, LINK 
+	// GUID, LANGUAGE, AUTHOR, PUB_DATE, COPYRIGHT] 
+	Feed parsed = newSource.readFeed();
+	// I NEED -> title, subtitle, link, feed, 
+
+	
+	String title = parsed.title;
+	String subTitle = parsed.description;
+	String link = parsed.link;
+	
+	
+	// this should insert this source into the DB..
+	Source s = new Source();
+	s.setFeed(url);
+	s.setLink(link);
+	s.setTitle(title);
+	s.setSubtitle(subTitle);
+	sourceRepository.save(s);
+	
+	return "Saved";
+
+	
+	
+}
+
+
+
+
 	
 }
